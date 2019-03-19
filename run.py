ea005192d68f8adf349f95472f8b8fbe6df18b98
= <=>
2019-03-19 16:30:46 +0200

    [UPDATE] initialise path

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
+
 class Initialise_path():
+
+    def __init__(self):
+        self.face  = "haarcascades/haarcascade_frontalface_default.xml"
+        self.eye   = "haarcascades/haarcascade_eye.xml"
+        self.image = "/home/gaelmmg/Pictures/IMG-20190220-WA0018.jpg"
