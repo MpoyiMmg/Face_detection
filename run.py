c9e604eb357ccc59c23b502320cb684466ecd28d
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-06-06 15:14:05 +0200

    [UPDATE] parcours de la faces pour y trouver les differents points

--- run.py
+++ run.py
@@ -8,3 +8,5 @@ resize_image = cv2.resize(image, (int(image.shape[1]/2), int(image.shape[0]/2)))
 gray_image = cv2.cvtColor(resize_image, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.05, minNeighbors=5)
+for x,y,w,h in faces:
+    image = cv2.rectangle(resize_image, (x,y), (x+w, y+h), (0,255,0), 3)
