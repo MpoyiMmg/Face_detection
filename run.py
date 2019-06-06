098cd00f14302443a492cce0b43b37910a6e7f94
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-06-06 15:07:48 +0200

    [UPDATE] mise a jour du chargement de l'image

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
 
-image = "images/mmg.jpg"
+image = cv2.imread( "images/mmg.jpg")
 resize_image = cv2.resize(image, (int(image.shape[1]/2), int(image.shape[0]/2)))
