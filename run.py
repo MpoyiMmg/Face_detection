4d971202ea08d7421b69ea038da8a095550d304e
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-06-06 15:12:17 +0200

    [UPDATE] appel de la methode detectMulticale pour

--- run.py
+++ run.py
@@ -4,3 +4,7 @@ face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_defau
 
 image = cv2.imread( "images/mmg.jpg")
 resize_image = cv2.resize(image, (int(image.shape[1]/2), int(image.shape[0]/2)))
+
+gray_image = cv2.cvtColor(resize_image, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.05, minNeighbors=5)
