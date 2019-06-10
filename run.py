485e3f0e6407452ee7b6df53fd662ea233db9216
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-06-10 12:55:46 +0200

    [UPDATE] mise a jour desc onditions des choix

--- run.py
+++ run.py
@@ -8,13 +8,16 @@ def main():
     print("1. Detecter sur une photo")
     print("2. Detecter a partir du webcam")
     print("0. Quitter")
-    
+
     choice = int(input("tapez  pour continuer..."))
 
-    if choice == 0 :
+    if choice == 1 :
 
         image_path = input("entrez le chemin de l'image a ce format : /chemin/vers/le/fichier.jpg : ")
         face_detect.detect_simple_image(image_path)
+
+    elif choice == 2:
+        face_detect.detect_by_video_capture()
     
     else :
         main()
