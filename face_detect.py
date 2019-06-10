098eb973675780b1ac0c40f9ea9aa6ccf6a3f742
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-06-10 13:02:26 +0200

    [UPDATE] finition de la video

--- face_detect.py
+++ face_detect.py
@@ -31,8 +31,14 @@ def detect_by_video_capture():
         _,img = video.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-        cv2.imshow("vide capture")
-        cv2.waitKey(0)
+        for(x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+        cv2.imshow("vide capture", img)
+        k =cv2.waitKey(30)
+        if k == 27:
+            break
+
     video.release()
