05119995610ba150e8a632cd6427a01a9af0effb
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-06-11 22:48:23 +0200

    [ADD] ajout de la fonctionalite de la capture de la face

--- face_recognition.py
+++ face_recognition.py
@@ -13,7 +13,19 @@ while(boolean):
     # lecture de l'image cadre par cadre
     ret, frame = cap.read()
 
+    # rendre l'image recue en noir blanc
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # lecture des echelles correspondant a une face
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
+    for(x,y,w,h) in faces:
+        # cv2.rectangle(frame, (x,y), (x+h, y+w), )
+        print(x,y,w,h)
+        
+        roi_gray = gray[y:y+h, x:x+w]
+        img_item = "ma_face.jpg"
+        cv2.imwrite(img_item, roi_gray)
     # affichage des resultats
     cv2.imshow('Frame',frame)
     
