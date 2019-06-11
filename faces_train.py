3c46cb05342a040be2ab7f8e7087d964f26c5c95
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-06-11 23:47:31 +0200

    [ADD] convertion de l'image en un tableau numpy

--- faces_train.py
+++ faces_train.py
@@ -1,11 +1,24 @@
 import os
+import numpy as np
+from PIL import Image
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
 
+y_labels = []
+x_train  =  []
+
 for root, dirs, files in os.walk(image_dir):
     for file in files:
         if file.endswith("png") or file.endswith("jpg") or file.endswith("jpeg"):
             path = os.path.join(root, file)
-            print(path)
+            label = os.path.basename(os.path.dirname(path)).replace(" ", "-").lower()
+            # print(label," : ",path)
+            y_labels.append(label)
+            x_train.append(path) #verifie l'image et la transforme en tableau numpy
+
+            pil_image = Image.open(path).convert("L") #grayscale
+            image_array = np.array(pil_image, "uint8")
+
+            print(image_array)
