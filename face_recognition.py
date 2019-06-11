cf1aad495ef5a0f3c59858940bc693b77320120c
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-06-11 22:22:33 +0200

    [ADD] ajout du code de prise d'image par la webcam

--- face_recognition.py
+++ face_recognition.py
@@ -2,3 +2,15 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
+boolean = True
+
+while(boolean):
+
+    ret, frame = cap.read()
+
+    cv2.imshow('Frame',frame)
+    
+    if cv2.waitKey(3) == ord('q'):
+        break
+
+cap.release()
