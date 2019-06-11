cf821583182b195d562dfe54127aa7627b2bec3a
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-06-11 22:37:38 +0200

    [ADD] un peu de commentaire d'explication

--- face_recognition.py
+++ face_recognition.py
@@ -1,15 +1,20 @@
 import numpy as np
 import cv2
 
+# ouverture de la webcam
 cap = cv2.VideoCapture(0)
 boolean = True
 
+# chargement du fichier cascade
 face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt2.xml')
 
 while(boolean):
 
+    # lecture de l'image cadre par cadre
     ret, frame = cap.read()
 
+
+    # affichage des resultats
     cv2.imshow('Frame',frame)
     
     if cv2.waitKey(3) == ord('q'):
