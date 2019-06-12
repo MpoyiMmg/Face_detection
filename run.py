974dca26d8f4dc60e050979d7a1b230ae75605e2
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-06-12 07:43:44 +0200

    [ADD] ajout des quelques commentaires

--- run.py
+++ run.py
@@ -13,14 +13,16 @@ def main():
 
     choice = int(input("tapez  pour continuer..."))
 
+    # premier choix est la detection du visage sur une image simple
     if choice == 1 :
-
         image_path = input("entrez le chemin de l'image a ce format : /chemin/vers/le/fichier.jpg : ")
         fd.detect_simple_image(image_path)
 
+    # deuxieme choix est la detection du visage a partir d'une webcam
     elif choice == 2:
         fd.detect_by_video_capture()
 
+    # troisieme choix est la Reconnaissance faciale a partir de la webcam
     elif choice == 3:
         fr.recognize()
 
