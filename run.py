d7734f91b259049e347a8be444afa0b963b0c030
MpoyiMmg <mpoyigaelmmg@gmail.com>
2019-06-12 07:37:57 +0200

    [UPDATE] mise a jour des selections des choix

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-import face_detect
+import face_detect as fd
+import face_recognition as fr
 
 print('---DETECTION DES VISAGES--------')
 
@@ -7,6 +8,7 @@ def main():
     print("------ Face Detect ---------")
     print("1. Detecter sur une photo")
     print("2. Detecter a partir du webcam")
+    print("3. Reconnaissance faciale")
     print("0. Quitter")
 
     choice = int(input("tapez  pour continuer..."))
@@ -14,10 +16,16 @@ def main():
     if choice == 1 :
 
         image_path = input("entrez le chemin de l'image a ce format : /chemin/vers/le/fichier.jpg : ")
-        face_detect.detect_simple_image(image_path)
+        fd.detect_simple_image(image_path)
 
     elif choice == 2:
-        face_detect.detect_by_video_capture()
+        fd.detect_by_video_capture()
+
+    elif choice == 3:
+        fr.recognize()
+
+    elif choice == 0:
+        exit
     
     else :
         main()
